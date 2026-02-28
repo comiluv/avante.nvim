56b6611814cbcea544941429356dac58c6128646
comiluv <choijaeho@gmail.com>
2026-02-28 08:39:21 +0000

    override Docker image with local code
    this is needed to mount py/rag-service/src directory to Docker image
    without rebuilding the image

--- lua/avante/rag_service.lua
+++ lua/avante/rag_service.lua
@@ -111,13 +111,15 @@ function M.launch_rag_service(cb)
       M.stop_rag_service()
     end
     local cmd_ = string.format(
-      "docker run --platform=linux/amd64 -d -p 0.0.0.0:%d:%d --name %s -v %s:/data -v %s:/host:ro -e TZ=%s -e ALLOW_RESET=TRUE -e DATA_DIR=/data -e RAG_EMBED_PROVIDER=%s -e RAG_EMBED_ENDPOINT=%s -e RAG_EMBED_API_KEY=%s -e RAG_EMBED_MODEL=%s -e RAG_EMBED_EXTRA=%s -e RAG_LLM_PROVIDER=%s -e RAG_LLM_ENDPOINT=%s -e RAG_LLM_API_KEY=%s -e RAG_LLM_MODEL=%s -e RAG_LLM_EXTRA=%s %s %s",
+      "docker run --platform=linux/amd64 -d -p 0.0.0.0:%d:%d --name %s -v %s:/data -v %s:/host:ro -v %s:/app/src:ro -e TZ=%s -e ALLOW_RESET=TRUE -e DATA_DIR=/data -e RAG_EMBED_PROVIDER=%s -e RAG_EMBED_ENDPOINT=%s -e RAG_EMBED_API_KEY=%s -e RAG_EMBED_MODEL=%s -e RAG_EMBED_EXTRA=%s -e RAG_LLM_PROVIDER=%s -e RAG_LLM_ENDPOINT=%s -e RAG_LLM_API_KEY=%s -e RAG_LLM_MODEL=%s -e RAG_LLM_EXTRA=%s %s %s",
       M.get_rag_service_port(),
       M.get_rag_service_port(),
       container_name,
       data_path,
       Config.rag_service.host_mount,
-	  tz,
+      jit.os == "Windows" and "C:\\code\\avante.nvim\\py\\rag-service\\src\\"
+        or "/mnt/c/code/avante.nvim/py/rag-service/src/",
+      tz,
       Config.rag_service.embed.provider,
       Config.rag_service.embed.endpoint,
       embed_api_key,
